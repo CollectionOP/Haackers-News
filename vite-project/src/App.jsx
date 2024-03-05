6899c7e8cc6333bda664cd71556c886e940f1d65
vladakosha <koshavlada@gmail.com>
2024-03-05 10:52:58 +0100

    footer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css"
 import Navbar from "./components/Navbar/Navbar.jsx"
+import Footer from "./components/Footer/Footer.jsx"
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    
     </>
   )
 }
