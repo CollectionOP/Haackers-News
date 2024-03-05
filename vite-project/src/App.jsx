69a25105e6049650fc526caf5f762b3fcbff5f0f
CollectionOP <alexapostol98@yahoo.com>
2024-03-05 14:51:36 +0100

    added api call

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,8 +2,33 @@ import "./App.css"
 import Navbar from "./components/Navbar/Navbar.jsx"
 import Footer from "./components/Footer/Footer.jsx"
 import Main from "./components/Main/Main.jsx"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 function App() {
+
+  const [news, setNews] = useState();
+  const [error, setErr] = useState();
+  const [isLoading, setIsLoading] = useState(true)
+
+  const newsApiCall = async () => {
+    try {
+      const response = await axios.get(`https://hn.algolia.com/api/v1/search_by_date?query=`)
+      setNews(response.data.hits)
+      // console.log(news);
+    }
+    catch (err){
+      setErr(err)
+      console.log(error);
+    }
+    finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    newsApiCall();
+  }, [])
 
   return (
     <>
