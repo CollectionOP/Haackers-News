feb1a3a673e993729274d9894ac993ef5a05327b
CollectionOP <alexapostol98@yahoo.com>
2024-03-05 10:21:02 +0100

    added tailwind

---
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+
+    return (
+        <header>
+            <nav>
+                <div>
+                    <ul>
+                        <a href=""><li className="text-red-800">Home</li></a>
+                        <a href=""><li>About</li></a>
+                        <a href=""><li>Contact</li></a>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
