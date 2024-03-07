577e0beb63acd64a5edae219329407de2dc56ff4
Samuelmindyobiz <Samuelolatidoye@yahoo.com>
2024-03-07 11:36:53 +0100

    Final commit

--- vite-project/src/components/Searchbar/Searchbar.jsx
+++ vite-project/src/components/Searchbar/Searchbar.jsx
@@ -1,18 +1,30 @@
 import { useState } from "react"
 
-const SearchBar = ({setNews, newsApiCall}) => {
+const SearchBar = ({ setNews, newsApiCall }) => {
     const [search, setSearch] = useState('')
+    const [page, setPage] = useState(0)
+
+    const handleNextPage = () => {
+        setPage(page + 1);
+    }
+
+    const handlePrevPage = () => {
+        setPage(page - 1);
+    };
+
+
 
     const handleClick = async () => {
+        setPage(0)
         await newsApiCall(search)
     }
-    return(
+    return (
         <>
-        <input type="submit" onClick={handleClick}></input>
-        <input placeholder="Search..." className="border-2" type="text" value={search} onChange={e => setSearch(e.target.value)}/>
+            <input type="submit" onClick={handleClick}></input>
+            <input placeholder="Search..." className="border-2" type="text" value={search} onChange={e => setSearch(e.target.value)} />
         </>
     )
-    
+
 }
 
 
